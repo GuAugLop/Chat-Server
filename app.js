f69f6c5a36a5996c3a23288c22c25b6610d3d9ab
Gustavo Lopes <56936953+Ted2370@users.noreply.github.com>
2021-02-04 15:44:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ io.on("connection", (socket) => {
       id,
       message,
       hour: (hour.getHours() < 10 ? `0${hour.getHours() - 3}` : hour.getHours() - 3),
-      minute: (minute.getMinutes() < 10 ? `0${minute.getMinutes() - 3}` : minute.getMinutes() - 3), 
+      minute: (minute.getMinutes() < 10 ? `0${minute.getMinutes()}` : minute.getMinutes()), 
     });
   });
   socket.on("disconnect", () => {
